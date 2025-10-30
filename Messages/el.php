ff8b4d634f3670033c037ae9841a83d740ca4891
Weblate <noreply@weblate.org>
2025-10-30 04:45:28 +0300

    Added translation using Weblate (Greek)

---
+++ Messages/el.php
@@ -0,0 +1,52 @@
+<?php
+return [
+    /**
+ * Copyright (C) MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Nikolay Beketov, 6 2018
+ *
+ */
+    'repModuleMonitorActiveCalls' => '',
+    'mo_ModuleModuleMonitorActiveCalls' => '',
+    'BreadcrumbModuleMonitorActiveCalls' => '',
+    'SubHeaderModuleMonitorActiveCalls' => '',
+    'module_template_AddNewRecord' => '',
+    'module_monitor_active_callsTextFieldLabel' => '',
+    'module_monitor_active_callsTextAreaFieldLabel' => '',
+    'module_monitor_active_callsPasswordFieldLabel' => '',
+    'module_monitor_active_callsIntegerFieldLabel' => '',
+    'module_monitor_active_callsCheckBoxFieldLabel' => '',
+    'module_monitor_active_callsToggleFieldLabel' => '',
+    'module_monitor_active_callsDropDownFieldLabel' => '',
+    'module_monitor_active_callsValidateValueIsEmpty' => '',
+    'module_monitor_active_callsConnected' => '',
+    'module_monitor_active_callsDisconnected' => '',
+    'module_monitor_active_callsUpdateStatus' => '',
+    'module_monitorCalls_Start' => '',
+    'module_monitorCalls_Who' => '',
+    'module_monitorCalls_Whom' => '',
+    'module_monitorCalls_joinAction' => '',
+    'module_monitorCalls_whisperAction' => '',
+    'module_monitorCalls_hangupAction' => '',
+    'module_monitorCalls_listenAction' => '',
+    'module_monitorCalls_Second' => '',
+    'module_monitorCalls_LoginTitle' => '',
+    'module_monitorCalls_userNumberTitle' => '',
+    'module_monitorCalls_CidTitle' => '',
+    'module_monitorCalls_userNumberNotFound' => '',
+    'module_monitorCalls_userNumberHelpMessage' => '',
+    'module_monitorCalls_waitingTitleClient' => '',
+    'module_monitorCalls_legendTitleInCall' => '',
+    'module_monitorCalls_legendTitleDisable' => '',
+    'module_monitorCalls_legendTitleIdle' => '',
+    'module_monitorCalls_columnTitleTalkTime' => '',
+    'module_monitorCalls_columnTitleWaitTime' => '',
+    'module_monitorCalls_columnTitleAgent' => '',
+    'module_monitorCalls_columnTitleAction' => '',
+    'module_monitorCalls_waitingClients' => '',
+    'module_monitorCalls_columnTitleSob' => '',
+    'module_monitorCalls_legendTitleSpy' => '',
+    'module_monitorCalls_needSelectQueue' => '',
+    'module_monitorCalls_legendTitleCalling' => '',
+];
